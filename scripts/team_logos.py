97e8fd5c2dc0a0bbea803d1b149d1ffb60910dee
Matthew Ikard <matthewikard@gmail.com>
2026-01-18 09:40:27 -0600

    add team logos script

---
+++ scripts/team_logos.py
@@ -0,0 +1,26 @@
+import cfbd
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+configuration = cfbd.Configuration(
+    access_token = os.getenv('CFBD_API_KEY')
+)
+
+logos_data = []
+
+with cfbd.ApiClient(configuration) as api_client:
+    api_instance = cfbd.TeamsApi(api_client)
+    logos = api_instance.get_teams(year=2025)
+    logos_data = [*logos, *logos_data]
+        
+
+logos_df = pd.DataFrame.from_records([dict(school = t.school,
+                                           color = t.color,
+                                           logo=t.logos[0] if t.logos else None) for t in logos_data])
+
+logos_df = logos_df.drop_duplicates()
+
+print(logos_df.head())
